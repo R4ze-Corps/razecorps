1b1e1795b3364c1c041d7505450f4532a8b9818b
R4ze-Corps <kawan123nogueira@gmail.com>
2026-05-03 13:06:24 -0300

    fix: update emoji IDs to latest provided and bump to M-1.8

--- src/discord/commands/public/ticket.ts
+++ src/discord/commands/public/ticket.ts
@@ -10,8 +10,8 @@ createCommand({
         const embed = new EmbedBuilder()
             .setColor("#2B2D31")
             .setDescription(
-                "<:Construcao:1326396621201866824> **Sistema de Atendimento - Raze Corporation**\n\n" +
-                "<:Ideia:1326680955225641021> **ATENÇÃO!**\n" +
+                "<:Construcao:1500525871965016064> **Sistema de Atendimento - Raze Corporation**\n\n" +
+                "<:Ideia:1500526811879182426> **ATENÇÃO!**\n" +
                 "Não abra um **ATENDIMENTO** sem ter algo relevante. Leia nossas [Termos & Condições](https://discord.com/channels/1320994106536759377/1321202554885115924)."
             )
             .setImage("https://r2.fivemanage.com/vLUsF9vzqBOo7DSFHERFX/Gemini_Generated_Image_giolfxgiolfxgiol(1).png");
